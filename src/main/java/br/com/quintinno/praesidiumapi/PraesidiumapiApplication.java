29744e07a6758d5d2641bfd6b27acd5930addf6c
Quintinno <josequintinno@icloud.com>
2024-09-20 11:55:02 -0300

    🟢 release: Configuração Inicial do Projeto

---
+++ src/main/java/br/com/quintinno/praesidiumapi/PraesidiumapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.praesidiumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraesidiumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraesidiumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quintinno/praesidiumapi/PraesidiumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.praesidiumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraesidiumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
