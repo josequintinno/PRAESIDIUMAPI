bbb5a7e8752ca3829d453dc59e9add0dc5c160bc
Quintinno <josequintinno@icloud.com>
2024-09-21 15:09:57 -0300

    🔵 feature: Implementar Gerar TOKEN JWT

--- src/main/java/br/com/quintinno/praesidiumapi/service/AutenticadorService.java
+++ src/main/java/br/com/quintinno/praesidiumapi/service/AutenticadorService.java
@@ -2,12 +2,16 @@ package br.com.quintinno.praesidiumapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.quintinno.praesidiumapi.repository.UsuarioImplementacaoRepository;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
 
 @Service
 public class AutenticadorService implements UserDetailsService {
@@ -24,7 +28,15 @@ public class AutenticadorService implements UserDetailsService {
         return this.usuarioImplementacaoRepository.recuperarUsuario(identificador);
     }
 
-    public String gerarJWT(Authentication authentication) {
+    public String gerarTOKENJWT(Authentication authentication) {
+
+        Instant instant = Instant.now();
+        Long prazoExpiracao = 36000L;
+
+        String escopo = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(" "));
+
+        JwtCla
+
         return null;
     }
 
